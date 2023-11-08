4944e9c4443f1435ec9384750c773b6d2ce153f4
hShamed <h.shamed.ar@gmail.com>
2023-11-08 15:25:18 -0600

    Creación del paquete DTO con su respectiva clase ToDo

--- src/main/java/com/todo/web/dto/ToDoDto.java
+++ src/main/java/com/todo/web/dto/ToDoDto.java
@@ -1,4 +1,17 @@
 package com.todo.web.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ToDoDto {
+    private Long Id;
+    private String text;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
