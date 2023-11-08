8d1b4653909d93d22eb826f9f34083bce47599f6
hShamed <h.shamed.ar@gmail.com>
2023-11-08 15:25:52 -0600

    Creación del paquete Services con su respectiva interfaz y clase ToDo

--- src/main/java/com/todo/web/services/ToDoService.java
+++ src/main/java/com/todo/web/services/ToDoService.java
@@ -1,4 +1,9 @@
 package com.todo.web.services;
 
+import com.todo.web.dto.ToDoDto;
+
+import java.util.List;
+
 public interface ToDoService {
+    List<ToDoDto> findAllToDos();
 }
--- src/main/java/com/todo/web/services/impl/ToDoServiceImpl.java
+++ src/main/java/com/todo/web/services/impl/ToDoServiceImpl.java
@@ -1,4 +1,39 @@
 package com.todo.web.services.impl;
 
-public class ToDoService implements ToDoService {
+import com.todo.web.dto.ToDoDto;
+import com.todo.web.models.ToDo;
+import com.todo.web.repositories.ToDoRepository;
+import com.todo.web.services.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ToDoServiceImpl implements ToDoService {
+
+    private ToDoRepository toDoRepository;
+
+    @Autowired
+    public ToDoServiceImpl(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+    @Override
+    public List<ToDoDto> findAllToDos() {
+        List<ToDo> toDos = toDoRepository.findAll();
+        return toDos.stream().map((toDo) -> mapToToDoDto(toDo)).collect(Collectors.toList());
+    }
+
+    private ToDoDto mapToToDoDto(ToDo toDo) {
+        ToDoDto toDoDto = ToDoDto.builder()
+                .Id(toDo.getId())
+                .text(toDo.getText())
+                .createdAt(toDo.getCreatedAt())
+                .updatedAt(toDo.getUpdatedAt())
+                .build();
+
+        return toDoDto;
+    }
 }
