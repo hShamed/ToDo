7945e43e89f863a8146ea93090b48ecbc8bfa7d9
hShamed <h.shamed.ar@gmail.com>
2023-11-09 15:08:36 -0600

    Creación del controlador para listar las tareas

--- src/main/java/com/todo/web/controllers/ToDoController.java
+++ src/main/java/com/todo/web/controllers/ToDoController.java
@@ -1,5 +1,30 @@
 package com.todo.web.controllers;
 
+import com.todo.web.dto.ToDoDto;
+import com.todo.web.services.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class ToDoController {
 
+    private ToDoService toDoService;
+
+    @Autowired
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping("/todo")
+    public String listToDos(Model model) {
+        List<ToDoDto> toDos = toDoService.findAllToDos();
+
+        model.addAttribute("toDos", toDos);
+
+        return "toDos-list";
+    }
 }
